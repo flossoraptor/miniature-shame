e222724c74e4501b190a292c28b4cbf42f3c3d9d
flossoraptor <sigle023@umn.edu>
2014-08-09 20:55:52 -0500

    parses text file containing map data to build area

--- miniature-shame/src/engine/AreaLoader.java
+++ miniature-shame/src/engine/AreaLoader.java
@@ -2,6 +2,8 @@ package engine;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 import assets.Area;
 import assets.Tile;
@@ -12,19 +14,65 @@ public class AreaLoader {
 		areas = new HashMap<>();
 		Area testArea = new Area(40, 30, 16, 16);
 		Tile[][] tiles = new Tile[40][30];
-		Tile ground = new Tile(spriteLoader.spriteMap.get("ground"));
-		Tile wall = new Tile(spriteLoader.spriteMap.get("wall"));
-		for (int i=0; i<tiles.length; i++) {
-			for (int j=0; j<tiles[i].length; j++) {
-				if (i==0 || j==0) {
-					tiles[i][j] = wall;
-				} else if (i==tiles.length-1 || j==tiles[i].length-1) {
-					tiles[i][j] = wall;
-				} else {
-					tiles[i][j] = ground;
-				}
-			}
+		Map<String, Tile> tileMap = new HashMap<>();
+		Tile greentl = new Tile(spriteLoader.spriteMap.get("greentl"));
+		Tile greentm = new Tile(spriteLoader.spriteMap.get("greentm"));
+		Tile greentr = new Tile(spriteLoader.spriteMap.get("greentr"));
+		Tile greenml = new Tile(spriteLoader.spriteMap.get("greenml"));
+		Tile greenmm = new Tile(spriteLoader.spriteMap.get("greenmm"));
+		Tile greenmr = new Tile(spriteLoader.spriteMap.get("greenmr"));
+		Tile greenbl = new Tile(spriteLoader.spriteMap.get("greenbl"));
+		Tile greenbm = new Tile(spriteLoader.spriteMap.get("greenbm"));
+		Tile greenbr = new Tile(spriteLoader.spriteMap.get("greenbr"));
+		Tile treetl = new Tile(spriteLoader.spriteMap.get("treetl"));
+		Tile treetr = new Tile(spriteLoader.spriteMap.get("treetr"));
+		Tile treebl = new Tile(spriteLoader.spriteMap.get("treebl"));
+		Tile treebr = new Tile(spriteLoader.spriteMap.get("treebr"));
+		Tile treethicktl = new Tile(spriteLoader.spriteMap.get("treethicktl"));
+		Tile treethicktr = new Tile(spriteLoader.spriteMap.get("treethicktr"));
+		Tile tinygrass = new Tile(spriteLoader.spriteMap.get("tinygrass"));
+		Tile greenflowers = new Tile(spriteLoader.spriteMap.get("greenflowers"));
+		tileMap.put("1", greentl);
+		tileMap.put("2", greentm);
+		tileMap.put("3", greentr);
+		tileMap.put("4", greenml);
+		tileMap.put("5", greenmm);
+		tileMap.put("6", greenmr);
+		tileMap.put("7", greenbl);
+		tileMap.put("8", greenbm);
+		tileMap.put("9", greenbr);
+		tileMap.put("10", treetl);
+		tileMap.put("11", treetr);
+		tileMap.put("12", treebl);
+		tileMap.put("13", treebr);
+		tileMap.put("14", treethicktl);
+		tileMap.put("15", treethicktr);
+		tileMap.put("16", tinygrass);
+		tileMap.put("17", greenflowers);
+
+		
+		try(BufferedReader in = new BufferedReader(new FileReader("resources/area/testareaexport.txt"))){
+		    String line;
+		    int xRead, yRead = 0;
+		    while ( (line=in.readLine()) != null) 
+		    {
+		    	xRead = 0;
+		        String[] values = line.split(",");
+		        for(String v:values)
+		        {
+		        	if (!v.equalsIgnoreCase(""))
+		        	{
+		        		tiles[xRead][yRead] = tileMap.get(v);
+		        		xRead++;
+		        	}
+		        }
+		        yRead++;
+		    }
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
 		}
+		
 		testArea.setTiles(tiles);
 		areas.put("testArea", testArea);
 	}
--- miniature-shame/src/engine/SpriteLoader.java
+++ miniature-shame/src/engine/SpriteLoader.java
@@ -18,10 +18,26 @@ public class SpriteLoader {
 		spriteMap = new HashMap<String, Sprite>();
 		
 		try {
-			Image tiles = new Image("/resources/tiles.png");
+			Image tiles = new Image("/resources/img/tiles.png");
 			SpriteSheet tileSheet = new SpriteSheet(tiles, 16, 16);
-			spriteMap.put("ground", new Sprite(tileSheet.getSprite(0, 0)));
-			spriteMap.put("wall", new Sprite(tileSheet.getSprite(1, 0)));
+			spriteMap.put("greentl", new Sprite(tileSheet.getSprite(0, 0)));
+			spriteMap.put("greentm", new Sprite(tileSheet.getSprite(1, 0)));
+			spriteMap.put("greentr", new Sprite(tileSheet.getSprite(2, 0)));
+			spriteMap.put("greenml", new Sprite(tileSheet.getSprite(3, 0)));
+			spriteMap.put("greenmm", new Sprite(tileSheet.getSprite(4, 0)));
+			spriteMap.put("greenmr", new Sprite(tileSheet.getSprite(5, 0)));
+			spriteMap.put("greenbl", new Sprite(tileSheet.getSprite(6, 0)));
+			spriteMap.put("greenbm", new Sprite(tileSheet.getSprite(7, 0)));
+			spriteMap.put("greenbr", new Sprite(tileSheet.getSprite(8, 0)));
+			spriteMap.put("treetl", new Sprite(tileSheet.getSprite(9, 0)));
+			spriteMap.put("treetr", new Sprite(tileSheet.getSprite(10, 0)));
+			spriteMap.put("treebl", new Sprite(tileSheet.getSprite(11, 0)));
+			spriteMap.put("treebr", new Sprite(tileSheet.getSprite(12, 0)));
+			spriteMap.put("treethicktl", new Sprite(tileSheet.getSprite(13, 0)));
+			spriteMap.put("treethicktr", new Sprite(tileSheet.getSprite(14, 0)));
+			spriteMap.put("tinygrass", new Sprite(tileSheet.getSprite(15, 0)));
+			spriteMap.put("greenflowers", new Sprite(new Animation(tileSheet, 16, 0, 19, 0, true, 500, true)));
+			
 		} catch (SlickException e) {
 			System.out.println("FAILED TO LOAD RESOURCE");
 			e.printStackTrace();
